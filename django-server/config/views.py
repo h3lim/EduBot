00acfad5ae27d8fdd129d585f65ba452c19efc22
NarciSource <jwch11@gmail.com>
2023-12-17 02:30:45 +0900

    feat: 기본 로그인 수정

--- django-server/config/views.py
+++ django-server/config/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def login(request):
-    return render(request, './login.html')
+    return render(request, './account/login.html')
 
 def index(request):
     return HttpResponse("빈 페이지")
