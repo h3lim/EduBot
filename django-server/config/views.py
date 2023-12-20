7e72a2a7857ad5c5f6b5831077486024cc657cb4
Awlstn <wlstn159159@naver.com>
2023-12-20 16:36:29 +0900

    로그인팝업v2

--- django-server/config/views.py
+++ django-server/config/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def login(request):
-    return render(request, './account/login.html')
+    return render(request, './account/index.html')
 
 def index(request):
     return HttpResponse("빈 페이지")
