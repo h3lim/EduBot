ab804a41c8e59f78bb936ff7a5ecc4d89bb4549f
yxxngjxn <yxxngjxn99@gmail.com>
2023-12-20 13:48:02 +0900

    chat-gpt link

--- django-server/chat/views.py
+++ django-server/chat/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 def chatpage(request):
 
     context = {
-        'chatbot': "https://f55981e5b3327c9ca8.gradio.live/",
+        'chatbot': "https://6daac81fc44a48e78e.gradio.live",
         }
 
     return render(request, './chat/index.html', context)
