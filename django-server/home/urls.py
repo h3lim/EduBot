5c0b870a766e0c21c05bd67cf77b5d9baf54e1de
Awlstn <wlstn159159@naver.com>
2023-12-21 13:57:10 +0900

    로그인팝업v6

--- django-server/home/urls.py
+++ django-server/home/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from . import views
 from django.shortcuts import render
 
-def popup(request):
-    return render(request, './account/popup.html')
-
 urlpatterns = [
     path('', views.index, name='home'),
-    path('popup/', popup, name='popup'),
 ]
