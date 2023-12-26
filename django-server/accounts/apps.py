0bcc7a372fede4dc030879549a2daf955dacc88b
NarciSource <jwch11@gmail.com>
2023-12-27 04:44:13 +0900

    feat: 소셜 계정 가입 시 프로필 이미지 제공

--- django-server/accounts/apps.py
+++ django-server/accounts/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        # signals.py 내의 신호 처리기를 임포트
+        import accounts.signals
---
+++ django-server/accounts/signals.py
@@ -0,0 +1,24 @@
+from allauth.account.signals import user_signed_up
+from django.dispatch import receiver
+from django.core.files.base import ContentFile
+import requests
+
+
+# 소셜 계정으로 가입시 시그널
+@receiver(user_signed_up)
+def populate_profile(request, sociallogin, **kwargs):
+    # user 모델에 소셜 계정 데이터 추가
+    user = sociallogin.account.user
+
+    # 구글 프로필 이미지
+    url = (sociallogin.account.extra_data.get('picture')
+           # 네이버 프로필 이미지
+           or sociallogin.account.extra_data.get('profile_image'))
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        user.avatar.save(user.username+'.jpg',
+                         # ContentFile을 사용하여 이미지 저장
+                         ContentFile(response.content), save=True)
+
+    user.save()
