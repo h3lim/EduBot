7ede99bbfb63e3d2190eb6dbeb2fcf7e80a37eee
NarciSource <jwch11@gmail.com>
2023-12-27 04:42:29 +0900

    feat: 꾸미기

--- django-server/config/views.py
+++ django-server/config/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def frontdoor(request):
     context = {
         # 배경 동영상
-        'background_video': "https://www.youtube.com/embed/Ga-UF1j7cQ4",
+        'background_video': "https://www.youtube.com/embed/Ga-UF1j7cQ4?mute=1&amp;loop=1&amp;autoplay=1&amp;rel=0&amp;controls=0&amp;showinfo=0",
     }
 
     return render(request, './frontdoor/index.html', context)
