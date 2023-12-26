ae4c12e5c094d2f86cb4ee789f284a5ff31ed351
Awlstn <wlstn159159@naver.com>
2023-12-26 22:36:30 +0900

    faq v1

--- django-server/home/urls.py
+++ django-server/home/urls.py
@@ -6,4 +6,5 @@ from accounts import views as accounts_view
 urlpatterns = [
     path('', views.index, name='home'),
     path('mypage', accounts_view.mypage, name='mypage'),
+    path('faq', accounts_view.faq, name='faq'),
 ]
