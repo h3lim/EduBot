bf58c833e9f8659712df326c62994b86f8c0e329
NarciSource <jwch11@gmail.com>
2023-12-27 23:12:32 +0900

    feat: 명언 롤링 추가

--- django-server/home/views.py
+++ django-server/home/views.py
@@ -7,6 +7,10 @@ def index(request):
     lectures = Lecture.objects.order_by('-visited')
     context = {
         'lectures': lectures,
+        'phrase_list': [
+            "반복은 천재를 낳고, 믿음은 기적을 낳는다. － 안진수",
+            "거인의 어깨에 올라서서 더 넓은 세상을 바라보라 － 지병규"
+        ],
     }
 
     return render(request, './home/index.html', context)
