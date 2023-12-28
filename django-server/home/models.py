b0fbf0add9278fb78f4f2f148497f5067047ae62
yxxngjxn <yxxngjxn99@gmail.com>
2023-12-28 16:34:55 +0900

    model 변수명 변경

--- django-server/home/models.py
+++ django-server/home/models.py
@@ -10,15 +10,14 @@ s3_storage = asset_storage.MediaStorage()
 # Create your models here.
 
 class Lecture(models.Model):
-    thumbnail = models.ImageField(
-        upload_to="images/", default="default_thumbnail.jpg", storage=s3_storage)
-    chapter = models.CharField(max_length=10, default="")
-    grade = models.SmallIntegerField(validators=[MinValueValidator(0),
-                                                 MaxValueValidator(5)], default=5)
-    visited = models.IntegerField(default=0)
     title = models.CharField(max_length=100, default="")
+    subject = models.CharField(max_length=10, default="")
+    teacher = models.CharField(max_length=10, default="")
+    thumbnail = models.ImageField(upload_to="images/", default="default_thumbnail.jpg", storage=s3_storage)
     summary = models.TextField(default="")
-    reamin_time = models.DurationField(default=timedelta)
+    student_count = models.IntegerField(default=0)
+    rating = models.SmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)], default=5)
+    remain_time = models.DurationField(default=timedelta)
 
     def __str__(self):
         return self.title
