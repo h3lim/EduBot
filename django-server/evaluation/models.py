80a4af75c9f086880bd02fa53173fdd5045ea53f
yxxngjxn <yxxngjxn99@gmail.com>
2023-12-29 11:06:26 +0900

    데이터베이스 테이블(Test) 추가

--- django-server/evaluation/models.py
+++ django-server/evaluation/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from lecture.models import Video
 
 # Create your models here.
+class Test(models.Model):
+    video = models.ForeignKey(Video, null=True, on_delete=models.CASCADE)
+    question = models.TextField(default="")
+    answer = models.TextField(default="")
+    
