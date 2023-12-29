4cf0d2c497fb35386ad5525a1a26b1dc8fd1f5e0
MooYaHo1 <wlq033@naver.com>
2023-12-29 14:04:12 +0900

    성취도 평가  v4

--- django-server/evaluation/urls.py
+++ django-server/evaluation/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.evaluation, name='evaluation'),
+    path('<int:video1>/', views.evaluation1, name='evaluation1'),
 ]
--- django-server/evaluation/views.py
+++ django-server/evaluation/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from .models import Test
 
 # Create your views here.
 
 
 def evaluation(request):
-    return render(request, "./evaluation/ev.html")
+    #시험 문제를 가져오기 -> 어느 비디오에서 왔는가? 
+    #시험 문제를 풀게하기
+    #채점까지 해야함.
+    #채점 결과
+    return render(request, "./evaluation/ev.html", context={})
+
+def evaluation1(request, video1):
+    print(video1)
+    qs = Test.objects.filter(video=video1)
+    for q in qs:
+        print(q.question)
+    #시험 문제를 가져오기 -> 어느 비디오에서 왔는가? 
+    #시험 문제를 풀게하기
+    #채점까지 해야함.
+    #채점 결과
+    return render(request, "./evaluation/ev.html", context={})
