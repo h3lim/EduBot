ddf08e8115383aff90cf29cbc8fa3cd9ab64e69f
pumple99 <ljhfree99@naver.com>
2023-12-31 00:51:38 +0900

    fix: evaluation 멀티스레드로 변경

--- ai/evalbot.py
+++ ai/evalbot.py
@@ -1,4 +1,5 @@
 import environ, os
+from .testbot import test_ai
 from openai import OpenAI
 
 
@@ -13,7 +14,7 @@ env = environ.Env(
 client = OpenAI(api_key=env('CHATGPT_API_KEY'))
 
 
-def make_message(my_ai_ans, true_ans):
+def make_message(my_ans, given_ans):
     messages = []
 
     # messages.append({"role": "system",
@@ -21,20 +22,24 @@ def make_message(my_ai_ans, true_ans):
     # messages.append({'role': 'assistant',
     #                  "content": "선생님, 오셨군요!"})
 
-    messages.append({"role": "user", "content": f'{my_ai_ans}, 이건 내가 쓴 답이고, {true_ans}, 이건 정답이야 내가 쓴 답을 채점해서 맞으면 1 틀리면 0 을 보내줘'})
+    messages.append({"role": "user", "content": f'{my_ans}, 이건 내가 쓴 답이고, {given_ans}, 이건 정답이야 내가 쓴 답을 채점해서 맞으면 1 틀리면 0 을 보내줘'})
     return messages
 
 
-def test_eval(my_ai_ans, true_ans):
+def test_eval(question, given_ans, eval_results, idx):
+    result = eval_results[idx]
+    result[0], result[1] = question, given_ans
+    my_ans = test_ai(question)
+    result[2] = my_ans
     try:
         response = client.chat.completions.create(
             model="gpt-4",
-            messages=make_message(my_ai_ans, true_ans),
+            messages=make_message(my_ans, given_ans),
             temperature=0.5,
         )
     except Exception as ex:
         if ex.code == "insufficient_quota":
-            answer = "죄송해요! API키 사용량 터졌어요!"
+            result[3] = "죄송해요! API키 사용량 터졌어요!"
     else:
-        answer = response.choices[0].message.content
-    return answer
+        result[3] = response.choices[0].message.content
+        
