af2874e01adff9075af42b97bae95f9fb0eb214d
pumple99 <ljhfree99@naver.com>
2023-12-31 00:51:38 +0900

    fix: evaluation 멀티스레드로 변경

--- django-server/evaluation/views.py
+++ django-server/evaluation/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from .models import Test
 from pathlib import Path
+from threading import Thread
 import sys
 big = Path(__file__).resolve().parent.parent.parent
 sys.path.append(str(big))
-from ai import testbot, evalbot
+from ai import evalbot
 # Create your views here.
 
 
@@ -12,26 +13,17 @@ def evaluation(request):
     return render(request, "./evaluation/ev.html", context={})
 
 def evaluation1(request, video1):
-    a = []
     qs = Test.objects.filter(video=video1)
-    for q in qs:
-        t = testbot.test_ai(q.question)
-        a.append(t)
-        print("답: ",t)
+    q_count = len(qs)
+    eval_results = [[0, 0, 0, 0] for i in range(q_count)]
 
-    checklist = []
-    for my, q in zip(a, qs):
-        is_answer = evalbot.test_eval(my, q.answer)
-        print('채점 결과: ', is_answer)
-        checklist.append(is_answer)
-    #시험 문제를 가져오기 -> 어느 비디오에서 왔는가? 
-    #시험 문제를 풀게하기
-    #채점까지 해야함.
-    #채점 결과
-    context = {
-        'num_correct': checklist.count('1'),
-        'num_wrong': checklist.count('0'),
-        'checklist': checklist,
-    }
+    threads = []
+    for idx, q in enumerate(qs):
+        threads.append(Thread(target=evalbot.test_eval, args=(q.question, q.answer, eval_results, idx)))
 
-    return render(request, "./evaluation/ev.html", context=context)
+    for th in threads: th.start()
+    for th in threads: th.join()
+
+    for er in eval_results:
+        print('문제:', er[0], '답:', er[1], '풀이:', er[2], '정답 여부:', er[3])
+    return render(request, "./evaluation/ev.html", context={})
