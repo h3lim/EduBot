62e644df5508f7b7a88baddb6aa5a23d9faabe83
NarciSource <jwch11@gmail.com>
2024-01-01 00:48:57 +0900

    feat: 채팅 데이터베이스에 저장

--- django-server/chat/consumers.py
+++ django-server/chat/consumers.py
@@ -1,9 +1,13 @@
-import sys, json
+import sys
+import json
 from channels.generic.websocket import WebsocketConsumer
 from pathlib import Path
+from lecture.models import Lecture
+from .models import Message
+from ai import chatbot
+
 big = Path(__file__).resolve().parent.parent.parent
 sys.path.append(str(big))
-from ai import chatbot
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -23,5 +27,12 @@ class ChatConsumer(WebsocketConsumer):
 
         print("query", message)
         print("answer", answer)
+
+        # 데이터베이스에 저장
+        lecture_id = self.scope['url_route']['kwargs']['room_name']
+        lecture = Lecture.objects.get(id=lecture_id)
+        instance = Message(
+            lecture=lecture, user_message=message, bot_message=answer)
+        instance.save()
 
         self.send(text_data=json.dumps({"message": answer}))
--- django-server/chat/views.py
+++ django-server/chat/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 
 
 def chatpage(request, lecture_name):
+    lecture_id = request.POST['lecture_id'] if request.method == 'POST' else None
+
     context = {
-        'lecture_name': lecture_name
+        'lecture_name': lecture_name,
+        'lecture_id': lecture_id,
     }
     return render(request, "./chat/page.html", context)
