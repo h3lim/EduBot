2e12bce379fe0fd28431e3091a2e3a18c519ea51
yxxngjxn <yxxngjxn99@gmail.com>
2024-01-02 15:49:52 +0900

    embedding

--- django-server/chat/models.py
+++ django-server/chat/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from lecture.models import Lecture
+from pgvector.django import VectorField
 
 # Create your models here.
 
@@ -7,3 +8,5 @@ class Message(models.Model):
     lecture = models.ForeignKey(Lecture, on_delete=models.DO_NOTHING)
     user_message = models.TextField()
     bot_message = models.TextField()
+    user_message_embedded = VectorField(dimensions=1536)
+    bot_message_embedded = VectorField(dimensions=1536)
