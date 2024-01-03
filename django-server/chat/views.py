83be86d3db3bf278151e5dd6a1ed77bf1050e54c
NarciSource <jwch11@gmail.com>
2024-01-03 22:21:12 +0900

    feat: 채팅 히스토리

--- django-server/chat/views.py
+++ django-server/chat/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-
+from itertools import chain
+from .models import Message
 # Create your views here.
 
 
 def chatpage(request, lecture_name):
     lecture_id = request.POST['lecture_id'] if request.method == 'POST' else None
+    messages = Message.objects.filter(lecture_id=lecture_id)
+
+    history = [[{'type': 'user', 'message': message.user_message, 'time': ''},
+                {'type': 'bot',  'message': message.bot_message,  'time': ''}] for message in messages]
+    history = list(chain(*history))
 
     context = {
         'lecture_name': lecture_name,
         'lecture_id': lecture_id,
+        'history': history,
     }
     return render(request, "./chat/page.html", context)
