cd46120abe4282241c3dab242a6fbcabc252dc53
NarciSource <jwch11@gmail.com>
2024-01-04 22:22:41 +0900

    feat: 계정 삭제 기능추가

--- django-server/accounts/urls.py
+++ django-server/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from allauth.account.views import LoginView, LogoutView, PasswordResetView
-from .views import CustomSignupView, mypage, faq, consent
+from .views import CustomSignupView, mypage, faq, consent, delete
 
 # Create your views here.
 
@@ -12,6 +12,7 @@ urlpatterns = [
     path('password/reset/', PasswordResetView.as_view(), name='account_reset_password'),
     path('mypage', mypage, name='mypage'),
     path('faq/', faq, name='faq'),
-    path('consent/', consent, name='consent' ),
+    path('consent/', consent, name='consent'),
+    path('delete/', delete, name='delete_account'),
     path('', include('allauth.urls')),  # 오버라이딩할 때 순서 잘 생각하자...
 ]
--- django-server/accounts/views.py
+++ django-server/accounts/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django_countries import countries
 from allauth.account.views import SignupView
@@ -34,28 +37,17 @@ def mypage(request):
 
 
 def faq(request):
-    # # 접근 유저
-    # username = request.user
-    # # 접근 유저의 모델 읽기
-    # model = models.User.objects.get(username=username)
-
-    # if request.method == "POST":
-
-    #     # 추가 폼 데이터
-    #     print(request.POST)
-    #     # 추가 파일 데이터
-    #     print(request.FILES)
-
-    #     # 모델폼으로 수정
-    #     user_form = UserForm(request.POST, request.FILES, instance=model)
-
-    #     # 유효성 검사 후 저장
-    #     if user_form.is_valid():
-    #         user_form.save()
-
-    # context = {'user': model, 'countries': countries}
     return render(request, './home/faq.html')
 
-def consent(request):
 
+def consent(request):
     return render(request, 'account/consent.html')
+
+
+@login_required
+def delete(request):
+    if request.method == 'POST':
+        user = request.user
+        user.delete()
+
+    return HttpResponseRedirect(reverse('frontdoor'))
