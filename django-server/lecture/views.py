1add6a8ddd73d4662db01d8355b828d2cb123e7d
MooYaHo1 <wlq033@naver.com>
2024-01-05 11:57:41 +0900

    cv(약력) 연동 완료

--- django-server/lecture/views.py
+++ django-server/lecture/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Lecture, Video
+from .models import Lecture, Video, User
 # Create your views here.
 
 
@@ -11,10 +11,15 @@ def lecture(request, lecture_name):
         lecture = Lecture.objects.get(id=request.POST['lecture_id'])
         videos = Video.objects.select_related('lecture').filter(lecture_id=lecture.id)
 
+
         for video in videos:
             lecture.remain_time += video.video_duration
+        
+        
+        
         context = {
             'lecture': lecture,
             'videos': videos,
+            'video_count' : len(videos)
         }
         return render(request, './lecture/index.html', context)
