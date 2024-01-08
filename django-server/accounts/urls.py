38bcbd44136caed756c533775c5ca08115a6db5e
NarciSource <jwch11@gmail.com>
2024-01-08 19:19:05 +0900

    bug fix:  회원가입 성/이름 칸

--- django-server/accounts/urls.py
+++ django-server/accounts/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('consent/', views.consent, name='consent'),
     path('delete/', views.delete, name='delete'),
     path('readmessage/', views.read_message, name='readmessage'),
-    path('', include('allauth.urls')),  # 오버라이딩할 때 순서 잘 생각하자...
 ]
