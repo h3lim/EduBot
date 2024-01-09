26ae8bf3f954c2a10071f4e48c17236cd0e57b8f
pumple99 <ljhfree99@naver.com>
2024-01-09 17:32:25 +0900

    feat: admin으로 메시지 확인 기능 추가

--- django-server/chat/admin.py
+++ django-server/chat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Message
 
 # Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'video', 'user_message', 'bot_message')
+
+admin.site.register(Message, MessageAdmin)
