0777c20c38369b593d933f6e10b94fcd14af3db3
NarciSource <jwch11@gmail.com>
2024-01-09 13:19:53 +0900

    feat: 그래프 연결

--- django-server/evaluation/views.py
+++ django-server/evaluation/views.py
@@ -1,20 +1,23 @@
+import re
+import json
 from django.shortcuts import render
 from lecture.models import Video
 from chat.models import Message
 from .models import TestResult
 from threading import Thread
 from config.settings import chatbot
-
+from accounts.models import User
+from django.core import serializers
 # Create your views here.
-
-
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 def evaluation(request, lecture_name, video_name):
     if request.method == 'POST':
         user_id = request.POST['user_id']
         video_id = request.POST['video_id']
 
         # 메시지검색
-        chat_messages = Message.objects.filter(video=video_id)
+        chat_messages = Message.objects.filter(user=user_id, video=video_id)
 
         # 문제지 & 정답지
         statements = Video.objects.get(id=video_id).testpapers.all()
@@ -37,8 +40,9 @@ def evaluation(request, lecture_name, video_name):
         # 결과
         score, correct_count, wrong_count = 0, 0, 0
         explanations = []
+        scores = []
         for er in eval_results:
-            print(*map(': '.join, zip(["문제", "답", "풀이", "점수 및 보완할 부분"], er)))
+            print(*map(': '.join, zip(["문제", "답", "풀이", "점수 및 보완할 부분"], er)), sep='\n')
             idx = er[3].find(':')
             point, explain = er[3][:idx], er[3][idx+1:]
             explanations.append(explain)
@@ -47,19 +51,28 @@ def evaluation(request, lecture_name, video_name):
             if point >= 70: correct_count += 1
             else: wrong_count += 1
             score += point
+            scores.append(point)
         score //= len(eval_results)
         
-        # Database 저장
-        instance = TestResult(score)
-        # 데이터베이스에 저장
-        instance.save()
+        user = User.objects.get(id=user_id)
+        for testpaper, score in zip(statements, scores):
+            instance = TestResult(testpaper=testpaper, score=score, user=user)
+            # 데이터베이스에 저장
+            instance.save()
+
+        test_results = TestResult.objects.filter(user=user)
+        fields_data = [{'evaluation_date': obj.evaluation_date, 'score': obj.score} for obj in test_results]
+        json_result = json.dumps(fields_data, cls=DjangoJSONEncoder)
+
+        evals = [{'score': score, 'explation': explation} for score, explation in zip(scores, explanations)]
 
         context = {
             'lecture_name': lecture_name,
             'video_name': video_name,
             'num_correct': correct_count,
             'num_wrong': wrong_count,
-            'checklist': explanations,
+            'detail_eval': json.dumps(evals),
+            'history': json_result,
         }
     else:
         context = {
